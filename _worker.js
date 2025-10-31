6d46db816da1b01a0e8e01bec019de4d53b76447
imambadrey-ai <imambadrey@gmail.com>
2025-11-01 04:29:26 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,8 +1,8 @@
 import { connect } from "cloudflare:sockets";
 
 // Variables
-let serviceName = "";
-let APP_DOMAIN = "";
+let serviceName = "pages.dev";
+let APP_DOMAIN = "proxy-aez";
 
 let prxIP = "";
 let cachedPrxList = [];
